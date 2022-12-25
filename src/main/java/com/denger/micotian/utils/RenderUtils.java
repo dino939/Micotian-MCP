e0436ed73d050e1322b38ad5748ce97b40fedcd0
Denger <oledenant777@gmail.com>
2022-12-25 03:35:48 +0100

    RebderUtil

--- src/main/java/com/denger/micotian/utils/RenderUtils.java
+++ src/main/java/com/denger/micotian/utils/RenderUtils.java
@@ -27,6 +27,33 @@ public final class RenderUtils {
         GL11.glColor4d((double)((float)color.getRed() / 255.0F), (double)((float)color.getGreen() / 255.0F), (double)((float)color.getBlue() / 255.0F), (double)((float)alpha / 255.0F));
 
     }
+    public static void drawGradientRectY(final float n, final float n2, final float n3, final float n4, final int n5, final int n6) {
+        final float n7 = (n5 >> 24 & 0xFF) / 255.0f;
+        final float n8 = (n5 >> 16 & 0xFF) / 255.0f;
+        final float n9 = (n5 >> 8 & 0xFF) / 255.0f;
+        final float n10 = (n5 & 0xFF) / 255.0f;
+        final float n11 = (n6 >> 24 & 0xFF) / 255.0f;
+        final float n12 = (n6 >> 16 & 0xFF) / 255.0f;
+        final float n13 = (n6 >> 8 & 0xFF) / 255.0f;
+        final float n14 = (n6 & 0xFF) / 255.0f;
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.shadeModel(7425);
+        final Tessellator instance = Tessellator.getInstance();
+        final BufferBuilder buffer = instance.getBuffer();
+        buffer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        buffer.pos(n3, n2, 0.0).color(n8, n9, n10, n7).endVertex();
+        buffer.pos(n, n2, 0.0).color(n8, n9, n10, n7).endVertex();
+        buffer.pos(n, n4, 0.0).color(n12, n13, n14, n11).endVertex();
+        buffer.pos(n3, n4, 0.0).color(n12, n13, n14, n11).endVertex();
+        instance.draw();
+        GlStateManager.shadeModel(7424);
+        GlStateManager.disableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableTexture2D();
+    }
 
     public static void drawColorBox(AxisAlignedBB axisalignedbb, float red, float green, float blue, float alpha) {
         Tessellator ts = Tessellator.getInstance();
